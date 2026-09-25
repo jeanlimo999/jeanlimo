fcfccd73a17571803fc5dfc59a2a3bc2c150a3a0
jeanlimo999 <jeanlimo.com@gmail.com>
2026-09-25 12:05:12 -0500

    Require PIN login before opening Dispatch

--- public/config.js
+++ public/config.js
@@ -2,6 +2,8 @@ window.JL_CONFIG = {
   company: "Jean Limo",
   ownerName: "Cash",
   ownerPhone: "2819170085",
+  dispatchPin: "0085",
+  extraDispatchPins: ["0929"],
   supabaseUrl: localStorage.getItem("jl_supabase_url") || "https://zfsthgozqowubpmscibd.supabase.co",
   supabaseAnonKey: localStorage.getItem("jl_supabase_key") || ""
 };
