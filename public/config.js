47a5ca9febc99fa87e3cc87fce868a16ea9c97c7
jeanlimo999 <jeanlimo.com@gmail.com>
2026-09-24 22:19:49 -0500

    Driver app: connect Supabase on phone so assigned jobs show

--- public/config.js
+++ public/config.js
@@ -2,6 +2,6 @@ window.JL_CONFIG = {
   company: "Jean Limo",
   ownerName: "Cash",
   ownerPhone: "2819170085",
-  supabaseUrl: localStorage.getItem("jl_supabase_url") || "",
+  supabaseUrl: localStorage.getItem("jl_supabase_url") || "https://zfsthgozqowubpmscibd.supabase.co",
   supabaseAnonKey: localStorage.getItem("jl_supabase_key") || ""
 };
